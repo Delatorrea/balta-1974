1a76ebbf2ecb69d258438d7c69ef5ccd611ce229
Delatorrea <emerson@delatorre.dev>
2021-08-06 19:27:33 -0300

    Criadas entidades de dominio do StoreContext

---
+++ src/DelatorreStore.Domain/StoreContext/Entities/Customer.cs
@@ -0,0 +1,12 @@
+namespace DelatorreStore.Domain.StoreContext.Entities
+{
+    public class Customer 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Document { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Adress { get; set; }
+    }
+}
